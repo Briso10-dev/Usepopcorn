35e17f9fe0037610bc8c855de51acf2973e0d9cf
Briso10-dev <kouambrice10@gmail.com>
2025-11-23 15:30:34 +0100

    logical separation of WatchedSummary component

--- src/App.js
+++ src/App.js
@@ -161,10 +161,6 @@ function WatchBox(){
   const [watched, setWatched] = useState(tempWatchedData);
   const [isOpen2, setIsOpen2] = useState(true);
 
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
-
   return (
      <div className="box">
           <button
@@ -175,7 +171,22 @@ function WatchBox(){
           </button>
           {isOpen2 && (
             <>
-              <div className="summary">
+             <WatchBox  watched={watched} />
+             
+            </>
+          )}
+        </div>  
+
+  )
+}
+
+function WatchedSummary({watched }){
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+  return(
+     <div className="summary">
                 <h2>Movies you watched</h2>
                 <div>
                   <p>
@@ -196,32 +207,6 @@ function WatchBox(){
                   </p>
                 </div>
               </div>
-
-              <ul className="list">
-                {watched.map((movie) => (
-                  <li key={movie.imdbID}>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                    <h3>{movie.Title}</h3>
-                    <div>
-                      <p>
-                        <span>⭐️</span>
-                        <span>{movie.imdbRating}</span>
-                      </p>
-                      <p>
-                        <span>🌟</span>
-                        <span>{movie.userRating}</span>
-                      </p>
-                      <p>
-                        <span>⏳</span>
-                        <span>{movie.runtime} min</span>
-                      </p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-        </div>  
-
   )
 }
+
