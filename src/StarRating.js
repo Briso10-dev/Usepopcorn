08ed3a7cc5ef85c6efc3ac85d7d7b97e17633566
Briso10-dev <kouambrice10@gmail.com>
2025-11-30 13:15:42 +0100

    dynamic rendering odf the stars

---
+++ src/StarRating.js
@@ -0,0 +1,13 @@
+
+export default function StarRating(){
+    return(
+        <div>
+            <div>
+                {Array.from({length: 5}, (_, i) => (
+                    <span>s{i+1}</span>
+                ))}
+            </div>
+            <p>10</p>
+        </div>
+    )
+}
