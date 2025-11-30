cf8e54c2458c18ce816b8efb26fd93cdbc399ecb
Briso10-dev <kouambrice10@gmail.com>
2025-11-30 13:32:08 +0100

    Style of all sub elements of componet

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,13 +1,29 @@
+const containerStyle = {
+        display : "flex",
+        alignItems: "center",
+        gap: "16px"
+};
+
+const starContainerStyle = {
+    display : "flex",
+    gap : "4px"
+}
+
+const textStyle = {
+    lineHeight : "1",
+    margin : "0",
+
+}
 
 export default function StarRating(){
     return(
-        <div>
-            <div>
+        <div style={containerStyle}>
+            <div style={starContainerStyle} >
                 {Array.from({length: 5}, (_, i) => (
                     <span>s{i+1}</span>
                 ))}
             </div>
-            <p>10</p>
+            <p style={textStyle}>10</p>
         </div>
     )
 }
