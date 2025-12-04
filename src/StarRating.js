0830f343e21b759719caab727085a99e747a0ee3
Briso10-dev <kouambrice10@gmail.com>
2025-12-04 20:48:18 +0100

    All props of in and out event configured

--- src/StarRating.js
+++ src/StarRating.js
@@ -18,7 +18,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) { //very common way in setting default props in React apps
     const [rating, setRating] = useState(1)
-    const [tempRating, setTemprating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
 
     function handleRating(rating) {
         setRating(rating);
@@ -28,9 +28,13 @@ export default function StarRating({ maxRating = 5 }) { //very common way in set
         <div style={containerStyle}>
             <div style={starContainerStyle} >
                 {Array.from({ length: maxRating }, (_, i) => (
-                    <Star key={i} 
+                    <Star 
+                    key={i} 
                     onRate={() => handleRating(i + 1)} 
-                    full={rating >= i +1}/>
+                    full={rating >= i +1}
+                    onHoverIn={() =>  setTempRating(i+1)}
+                    onHoverOut={() => setTempRating(0)}
+                    />
                 ))}
             </div>
             <p style={textStyle}>{rating || ""}</p>
@@ -45,11 +49,11 @@ const starStyle = {
     cursor: 'pointer'
 }
 
-function Star({ onRate, full }) {
+function Star({ onRate, full,onHoverIn,onHoverOut }) {
     return (
         <span role="button" style={starStyle} onClick={onRate}
-        onMouseEnter={()=>console.log("Enter")}
-        onMouseLeave={()=>console.log("Leave")}    
+        onMouseEnter={onHoverIn}
+        onMouseLeave={onHoverOut}    
         >
             {full ? ( 
                 <svg
