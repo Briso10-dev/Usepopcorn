ca17bde3b8f184a724d90bb3c70bed389ed4709b
Briso10-dev <kouambrice10@gmail.com>
2025-12-07 22:20:26 +0100

    Hovering with fill stars effects

--- src/StarRating.js
+++ src/StarRating.js
@@ -31,7 +31,7 @@ export default function StarRating({ maxRating = 5 }) { //very common way in set
                     <Star 
                     key={i} 
                     onRate={() => handleRating(i + 1)} 
-                    full={rating >= i +1}
+                    full={tempRating? tempRating >= i+1 : rating >= i +1}
                     onHoverIn={() =>  setTempRating(i+1)}
                     onHoverOut={() => setTempRating(0)}
                     />
