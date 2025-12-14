9a10291d3629ee139ae0c1c6f2d0214d0eac40e2
Briso10-dev <kouambrice10@gmail.com>
2025-12-14 18:53:55 +0100

    Rendering starRating component  more reusable

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={5} />
+    <StarRating maxRating={10} color='blue' />
     {/* <App /> */}
   </React.StrictMode>
 );
