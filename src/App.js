0647c88f170932f2c7109fd9a3ab3dbd0dcac989
Briso10-dev <kouambrice10@gmail.com>
2025-12-30 23:41:59 +0100

    Synchronizing queries with movie data

--- src/App.js
+++ src/App.js
@@ -50,110 +50,143 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-  const KEY = 'c7ccf0a6'
+const KEY = 'c7ccf0a6'
 
 //structured component
-export default function App() {   
-  const [movies, setMovies] = useState([]); 
+export default function App() {
+  const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
-  const query = "interstellar";
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] =  useState("")
+  const query = "qwertyuirtyui";
 
-  useEffect( function() {
+  useEffect(function () {
     async function fetchMovies() {
-      const res = await fetch(` http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
-    
-      const data = await res.json();
-      setMovies(data.Search)
-      console.log(movies)
+      try {
+        setIsLoading(true);
+        const res = await fetch(` http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+
+        if ( !res.ok)
+          throw new Error("Something went wrong with fetching movies")
+
+        // case of no film search foundç
+        const data = await res.json();
+        if (data.Response === "False") throw new Error ("Movie not found")
+
+        setMovies(data.Search);
+        console.log(data);
+      } catch (err) {
+        console.error(err.message);
+        setError(err.message)
+      } finally {
+        setIsLoading(false)
+      }
     }
     fetchMovies();
-    }, []); 
-  
+  }, []);
+
   return (
-    <>     
+    <>
       <NavBar>
-         <Search />
-          <NumResults movies={movies}/> 
-       </NavBar>  
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
       <Main>
         <Box>
-            <MovieList movies={movies} />
+          {/* 3 mutuallally exclusive conditions : only one can be true at the same time */}
+          {!isLoading && !error && <MovieList movies= {movies} />}
+          {error && <ErrorMessage message={error} />}
         </Box>
         <Box >
-             <WatchedSummary watched={watched} />
-             <WatchedMoviesList watched={watched} /> 
-        </Box>  
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
       </Main>
     </>
   );
 }
+
+function Loader() {
+  return (
+    <p className="loader">Loading...</p>
+  )
+}
+
+function ErrorMessage({message}){
+    return (
+      <p className="error">
+        <span>{message}</span>
+      </p>
+    )
+}
+
 //structured component . responsible for the structure and the layout of the app.
-function NavBar({ children }){
-  
+function NavBar({ children }) {
+
   return (
     <nav className="nav-bar">
-       <Logo />
-       {children}
-      </nav>
-  
+      <Logo />
+      {children}
+    </nav>
+
 
   )
 }
 //stateless component
-function Logo(){
-    return(
-      <div className="logo">
-          <span role="img">🍿</span>
-          <h1>usePopcorn</h1>
-        </div>
-    )
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  )
 }
 //stateful component
-function Search(){
+function Search() {
   const [query, setQuery] = useState("");
-  return(
+  return (
     <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
 
   )
 }
 //stateless/presentational component
-function NumResults({movies}){
-  return(
-       <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
+function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
   )
 }
 
-function Main({children}){
+function Main({ children }) {
 
-  return(
-       <main className="main">
-       {children}
-      </main>
- 
+  return (
+    <main className="main">
+      {children}
+    </main>
+
   )
 }
 //stateful component
-function Box({children}){
+function Box({ children }) {
   const [isOpen, setIsOpen] = useState(true);
 
-  return(
+  return (
     <div className="box">
-          <button
-            className="btn-toggle"
-            onClick={() => setIsOpen((open) => !open)}
-          >
-            {isOpen ? "–" : "+"}
-          </button>
-          {isOpen && children}
-        </div>
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen((open) => !open)}
+      >
+        {isOpen ? "–" : "+"}
+      </button>
+      {isOpen && children}
+    </div>
   )
 }
 /*stateful component
@@ -182,93 +215,93 @@ function WatchBox(){
 }
 */
 //stateful component
-function MovieList({movies}){
+function MovieList({ movies }) {
 
-  return(
-     <ul className="list">
-              {movies?.map((movie) => (
-               <Movie movie={movie} key={movie.imdbID} />
-              ))}
-            </ul>
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
   )
 }
 //stateless component
-function Movie({ movie }){
-  return(
-     <li key={movie.imdbID}>
-                  <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                  <h3>{movie.Title}</h3>
-                  <div>
-                    <p>
-                      <span>🗓</span>
-                      <span>{movie.Year}</span>
-                    </p>
-                  </div>
-                </li>
+function Movie({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
   )
 }
 
 //stateless component
-function WatchedSummary({watched }){
+function WatchedSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
 
-  return(
-     <div className="summary">
-                <h2>Movies you watched</h2>
-                <div>
-                  <p>
-                    <span>#️⃣</span>
-                    <span>{watched.length} movies</span>
-                  </p>
-                  <p>
-                    <span>⭐️</span>
-                    <span>{avgImdbRating}</span>
-                  </p>
-                  <p>
-                    <span>🌟</span>
-                    <span>{avgUserRating}</span>
-                  </p>
-                  <p>
-                    <span>⏳</span>
-                    <span>{avgRuntime} min</span>
-                  </p>
-                </div>
-              </div>
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
+      </div>
+    </div>
   )
 }
 //stateless component
-function WatchedMoviesList({watched}){
-  return(
-       <ul className="list">
-                {watched.map((movie) => (
-                <WatchMovie movie={movie} key={movie.imdbID} />
-                ))}
-              </ul>
+function WatchedMoviesList({ watched }) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchMovie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
   )
 }
 //stateless component
-function WatchMovie({movie}){
-  return(
-       <li key={movie.imdbID}>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                    <h3>{movie.Title}</h3>
-                    <div>
-                      <p>
-                        <span>⭐️</span>
-                        <span>{movie.imdbRating}</span>
-                      </p>
-                      <p>
-                        <span>🌟</span>
-                        <span>{movie.userRating}</span>
-                      </p>
-                      <p>
-                        <span>⏳</span>
-                        <span>{movie.runtime} min</span>
-                      </p>
-                    </div>
-                  </li>
+function WatchMovie({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>
   )
-   
+
 }
