76543c3de7b3ffd5d6397f80777ae62158fe1aaf
Briso10-dev <kouambrice10@gmail.com>
2025-12-30 16:40:23 +0100

    Error handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,11 +50,25 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+  const KEY = 'c7ccf0a6'
+
 //structured component
 export default function App() {   
-  const [movies, setMovies] = useState(tempMovieData); 
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]); 
+  const [watched, setWatched] = useState([]);
+  const query = "interstellar";
 
+  useEffect( function() {
+    async function fetchMovies() {
+      const res = await fetch(` http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+    
+      const data = await res.json();
+      setMovies(data.Search)
+      console.log(movies)
+    }
+    fetchMovies();
+    }, []); 
+  
   return (
     <>     
       <NavBar>
@@ -123,7 +137,7 @@ function Main({children}){
        <main className="main">
        {children}
       </main>
-
+ 
   )
 }
 //stateful component
